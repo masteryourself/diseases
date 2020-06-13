db570bff0dab064279c1449337aeaf9463937920
masteryourself <renzhao.ruan@foxmail.com>
2020-06-13 23:30:03 +0800

    案例修改

--- diseases-spring-cloud/diseases-spring-cloud-feign-listener/src/main/java/pers/masteryourself/diseases/spring/cloud/feign/listener/component/MyApplicationListener.java
+++ diseases-spring-cloud/diseases-spring-cloud-feign-listener/src/main/java/pers/masteryourself/diseases/spring/cloud/feign/listener/component/MyApplicationListener.java
@@ -1,7 +1,5 @@
 package pers.masteryourself.diseases.spring.cloud.feign.listener.component;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -22,26 +20,29 @@ public class MyApplicationListener implements ApplicationListener<ContextRefresh
 
     private final AtomicInteger count = new AtomicInteger(0);
 
-    @Autowired
-    private SomeBean someBean;
 
+    /***********************************    场景一   ***********************************/
     @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        // 初始化操作，只能做一次，但实际它会被调用多次
+        System.out.println("做了一件非常重要的事情，且只能初始化一次");
+    }
+
+
+    /***********************************    场景二   ***********************************/
+    /*@Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         String displayName = event.getApplicationContext().getDisplayName();
         // 第[1]次调用，context 上下文是：FeignContext-baidu
         // 第[2]次调用，context 上下文是：FeignContext-csdn
         // 第[3]次调用，context 上下文是：org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext@7d3e8655
         System.out.println("第[" + count.incrementAndGet() + "]次调用，context 上下文是：" + displayName);
-        // someBean 是否被初始化完成：pers.masteryourself.diseases.spring.cloud.feign.listener.component.SomeBean@6dba847b
-        // someBean 是否被初始化完成：pers.masteryourself.diseases.spring.cloud.feign.listener.component.SomeBean@6dba847b
-        // someBean 是否被初始化完成：pers.masteryourself.diseases.spring.cloud.feign.listener.component.SomeBean@6dba847b
-        System.out.println("someBean 是否被初始化完成：" + someBean);
         // 仅仅适用于 spring cloud F 版本之后，F 版本之前可使用 AtomicBoolean 来判断（因为没有设置 displayName）
         if (displayName.startsWith(FeignContext.class.getSimpleName())) {
             return;
         }
         // 初始化操作，只能做一次
-        someBean.init();
-    }
+        System.out.println("做了一件非常重要的事情，且只能初始化一次");
+    }*/
 
 }
